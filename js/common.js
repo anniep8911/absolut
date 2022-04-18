4dd175e644211ce9b120c7e882d4a345662c6ad2
anniep8911 <pjatopazz@gmail.com>
2022-04-18 23:10:56 +0900

    ver01

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function(){
+    var wd = $('html').width();
+    var hdrWrap =  $('.hdrWrap');
+    var ftrWrap = $('.ftrWrap');
+    hdrWrap.load('../inc/head.html',function(){});
+    ftrWrap.load('../inc/foot.html',function(){});
+
+    if(wd >= 780){
+        $('head').append('<script src=\'js/index.js\'><\/script>');
+    }else{  
+        $('head').append('<script src=\'js/jquery.mobile-1.4.5.min.js\'><\/script>');      
+        $('head').append('<script src=\'js/mob_index.js\'><\/script>');      
+    }
+});
