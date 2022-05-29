0beeb1a3de8c4be845935ca763c0d5ae0eccc2cb
anniep8911 <pjatopazz@gmail.com>
2022-05-29 17:17:14 +0900

    ver2

--- js/common.js
+++ js/common.js
@@ -8,7 +8,7 @@ $(function(){
     if(wd >= 780){
         $('head').append('<script src=\'js/index.js\'><\/script>');
     }else{  
-        $('head').append('<script src=\'js/jquery.mobile-1.4.5.min.js\'><\/script>');      
+        $('head').append('<script src=\'js/jquery.mobile.custom.min.js\'><\/script>');      
         $('head').append('<script src=\'js/mob_index.js\'><\/script>');      
     }
 });
